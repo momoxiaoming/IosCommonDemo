8fb16a17c20f51b45e65a62a2fa236ec6b465194
245545357@qq.com <245545357@qq.com>
2019-12-26 15:45:11 +0800

    pod 提交

---
+++ Pods/Target Support Files/Pods-IosCommonDemo/Pods-IosCommonDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IosCommonDemo : NSObject
+@end
+@implementation PodsDummy_Pods_IosCommonDemo
+@end
---
+++ Pods/Target Support Files/Pods-IosCommonDemoUITests/Pods-IosCommonDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IosCommonDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IosCommonDemoUITests
+@end
